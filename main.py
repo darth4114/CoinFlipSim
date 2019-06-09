d027750921694096e0500323ad702321de4ebcf1
Dan W <50939662+darth4114@users.noreply.github.com>
2019-06-08 22:35:55 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import modules
+
+print("Welcome to the flip a coin game!")
+
+while True:
+
+	num = modules.ask()
+
+	results = modules.flip_count(num)
+
+	print(f"Over {num} fiips, the results are {results[0]} heads and {results[1]} tails!\n")
+
+	if input("Would you like to play again? (y/n): ").lower() == 'y':
+		continue
+	else:
+		print("Thanks for playing!")
+		break
---
+++ modules.py
@@ -0,0 +1,42 @@
+import random
+
+def coin_flip():
+	'''	Flips a coin, heads or tails. Takes no input'''
+	flip = random.randint(0,1)
+
+	if flip:
+		return "Heads"
+	else:
+		return "Tails"
+
+
+def ask():
+	'''	Asks for an integer value for how many times to flip a coin'''
+	while True:
+		try:
+			tries = int(input("\nHow many times would you like to flip the coin? "))
+		except:
+			print("Sorry, I did not understand you\n")
+			continue
+		else:
+			break
+
+	return tries
+
+
+def flip_count(flips):
+	'''	
+	Counts the instances of heads and tails over x amount of flips
+	args = number of flips
+	'''
+
+	heads = 0
+	tails = 0
+
+	for i in range(flips):
+		if coin_flip() == "Heads":
+			heads += 1
+		else:
+			tails += 1
+
+	return heads, tails
