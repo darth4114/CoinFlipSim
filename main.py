a578e67c22657990ad8f489605b83cc4aa55eb70
darth4114 <darthmaul4114@gmail.com>
2019-06-09 13:28:59 -0700

    merge tabs to spaces

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import modules
 
-print("Welcome to the flip a coin game!")
+print("Welcome to the flip a coin game! You can pick how many times you want to flip a coin, and this program will tell you how many were heads and how many were tails.\nLet's Begin!")
 
 while True:
 
-	num = modules.ask()
+    num = modules.ask()
 
-	results = modules.flip_count(num)
+    results = modules.flip_count(num)
 
-	print(f"Over {num} fiips, the results are {results[0]} heads and {results[1]} tails!\n")
+    print(
+        f"Over {num} fiips, the results are {results[0]} heads and {results[1]} tails!\n")
 
-	if input("Would you like to play again? (y/n): ").lower() == 'y':
-		continue
-	else:
-		print("Thanks for playing!")
-        print("This is an edit to test Git!")
-		break
+    if input("Would you like to play again? (y/n): ").lower() == 'y':
+        continue
+    else:
+        print("Thanks for playing!")
+        break
--- modules.py
+++ modules.py
@@ -1,42 +1,44 @@
 import random
 
-def coin_flip():
-	'''	Flips a coin, heads or tails. Takes no input'''
-	flip = random.randint(0,1)
 
-	if flip:
-		return "Heads"
-	else:
-		return "Tails"
+def coin_flip():
+    '''	Flips a coin, heads or tails. Takes no input'''
+    flip = random.randint(0, 1)
+
+    if flip:
+        return "Heads"
+    else:
+        return "Tails"
 
 
 def ask():
-	'''	Asks for an integer value for how many times to flip a coin'''
-	while True:
-		try:
-			tries = int(input("\nHow many times would you like to flip the coin? "))
-		except:
-			print("Sorry, I did not understand you\n")
-			continue
-		else:
-			break
+    '''	Asks for an integer value for how many times to flip a coin'''
+    while True:
+        try:
+            tries = int(
+                input("\nHow many times would you like to flip the coin? "))
+        except:
+            print("Sorry, I did not understand you\n")
+            continue
+        else:
+            break
 
-	return tries
+    return tries
 
 
 def flip_count(flips):
-	'''	
-	Counts the instances of heads and tails over x amount of flips
-	args = number of flips
-	'''
+    '''
+    Counts the instances of heads and tails over x amount of flips
+    args = number of flips
+    '''
 
-	heads = 0
-	tails = 0
+    heads = 0
+    tails = 0
 
-	for i in range(flips):
-		if coin_flip() == "Heads":
-			heads += 1
-		else:
-			tails += 1
+    for i in range(flips):
+        if coin_flip() == "Heads":
+            heads += 1
+        else:
+            tails += 1
 
-	return heads, tails
+    return heads, tails
